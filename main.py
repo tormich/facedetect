60083bd74b76048cf082cbd2cd73754cc8c805e1
Artem <artem.kutsiy@riskified.com>
2019-03-28 17:54:53 +0200

    works with preloaded picture of abba

--- main.py
+++ main.py
@@ -4,14 +4,37 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 from tornado.web import RequestHandler
-
+import cv2
+import sys
 
 PORT = 8080
 
 
 class FaceCount(RequestHandler):
     def get(self):
-        pass
+
+        # Get user supplied values
+        imagePath = "FaceDetect/abba.png"
+        cascPath = "FaceDetect/haarcascade_frontalface_default.xml"
+
+        # Create the haar cascade
+        faceCascade = cv2.CascadeClassifier(cascPath)
+
+        # Read the image
+        image = cv2.imread(imagePath)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the image
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+            # flags = cv2.CV_HAAR_SCALE_IMAGE
+        )
+
+        self.write("Found {0} faces!".format(len(faces)))
+
 
     def post(self):
         pass
